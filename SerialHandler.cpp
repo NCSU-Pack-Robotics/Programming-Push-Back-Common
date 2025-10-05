0dc2ab441ddc80319bcee0f838f9079cd08096e2
miles <mtdaly@ncsu.edu>
2025-10-05 17:15:11 -0400

    Changed file read/write to use libusb_bulk_transfer

--- SerialHandler.cpp
+++ SerialHandler.cpp
@@ -1,79 +1,111 @@
 #include "SerialHandler.hpp"
 
+#include <array>
 #include <cmath>
 #include <cstring>
 #include <iostream>
 #include <fcntl.h>
+#include <optional>
 #include <unistd.h>
+#include <vector>
+#include <libusb-1.0/libusb.h>
 
-#include "Buffer.hpp"
-#include "PacketId.hpp"
-
-SerialHandler::SerialHandler(const DeviceType device_type) : device_type(device_type) {
+SerialHandler::SerialHandler(const DeviceType device_type) : device_type(device_type), device_handle(nullptr) {
     if (device_type == DeviceType::PI) {
-
-        // TODO: Should implement something to scan for the correct /dev/ttyACMx port
-        // Open with read/write and O_NOCTTY is so we don't become the process's controlling terminal
-        this->fd = open("/dev/ttyACM1", O_RDWR | O_NOCTTY);
-
-        if (this->fd < 0) {
-            perror("Error opening file: ");
+        // Initialize the libusb context. We pass nullptr as the context to use the global default one.
+        if (int error = libusb_init_context(nullptr, nullptr, 0) != LIBUSB_SUCCESS)
+        {
+            printf("Failed to initialize libusb context: %s\n", libusb_error_name(error));
+            return;
         }
 
-        // TODO: Better error handling here, maybe continuously try to connect until it works
+        libusb_device** devices = nullptr;
+        // Get the list of devices on the system
+        ssize_t device_count = libusb_get_device_list(nullptr, &devices);
+        if (device_count < 0)
+        {
+            printf("Failed to get device list: %s\n", libusb_error_name(static_cast<int>(device_count)));
+            return;
+        }
+
+        // TODO: We should probably retry initializing the context and getting the device list if they ever fail.
+
+        // Loop the connected devices to find the VEX V5 Brain
+        for (int i = 0; i < device_count; i++)
+        {
+            libusb_device_descriptor device_descriptor{};
+            // Get the device descriptor. If LIBUSBX_API_VERSION >= 0x01000102 then this function will never fail.
+            if (libusb_get_device_descriptor(devices[i], &device_descriptor) < 0) continue;
+
+            if (device_descriptor.idVendor == VEX_USB_VENDOR_ID)
+            {
+                libusb_device_handle* handle = nullptr;
+                if (int error = libusb_open(devices[i], &handle) != LIBUSB_SUCCESS)
+                {
+                    printf("Failed to open VEX Brain: %s\n", libusb_error_name(error));
+                    // TODO: If we find it can error, we should make a loop to try to open multiple times
+                    return;
+                }
+                this->device_handle = handle;
+                break;
+            }
+        }
+
+        // All devices start with ref count of 1, this subtracts 1 so it dereferences them all
+        libusb_free_device_list(devices, 1);
     }
 }
 
 SerialHandler::~SerialHandler() {
-    if (this->fd != -1) {
-        close(fd);
+    if (this->device_handle)
+    {
+        libusb_close(this->device_handle);
+        libusb_exit(nullptr);
     }
 }
 
 void SerialHandler::receive() {
-    if (fd == -1) return;
+    uint8_t in = ' ';  // Current byte being read
+    std::vector<uint8_t> bytes;
 
-        uint8_t in = ' ';  // Current byte being read
-        std::vector<uint8_t> bytes;
-
-        // Read until we get a null byte (0x00)—end of packet
-        while (in != '\0') {
-            ssize_t num_read = 0;
-            if (this->device_type == DeviceType::PI) {
-                num_read = read(this->fd, &in, 1);
-            } else if (this->device_type == DeviceType::BRAIN) {
-                num_read = read(STDIN_FILENO, &in, 1);
-            }
-
-            if (num_read != 1) {
-                /* Error occurred or EOF */
-                return;
-            }
-
-            bytes.push_back(in);
+    // Read until we get a null byte (0x00)—end of packet
+    // TODO: It would be faster to read more than 1 byte at a time, but I'm not sure if the benefit outweighs the cost to search the data for the null byte.
+    while (in != '\0') {
+        ssize_t num_read = 0;
+        if (this->device_type == DeviceType::PI) {
+            libusb_bulk_transfer(this->device_handle, VEX_USB_USER_DATA_ENDPOINT_IN, &in, 1, reinterpret_cast<int*>(&num_read), 0);
+        } else if (this->device_type == DeviceType::BRAIN) {
+            num_read = read(STDIN_FILENO, &in, 1);
         }
 
-        // Cobs does not decode the last 0x00 byte, it is only used in encoding so we have a delimiter
-        bytes.pop_back();
-
-        const std::optional<std::vector<uint8_t>> decoded = cobs_decode(bytes);
-        if (!decoded.has_value()) return; // If we fail to decode, ignore the packet
-
-        // Decode the header
-        Header received_header{};
-        memcpy(&received_header, decoded->data(), sizeof(received_header));
-
-        Packet received_packet{received_header, decoded.value()};
-
-        // Ensure the packet is valid by checking the checksum
-        if (!received_packet.check_checksum()) {
-            // If the checksum is invalid, ignore the packet
+        if (num_read != 1) {
+            /* Error occurred or EOF */
             return;
         }
 
+        bytes.push_back(in);
+    }
 
-        this->buffers[received_header.packet_id].add(received_packet);
+    // Cobs does not decode the last 0x00 byte, it is only used in encoding so we have a delimiter
+    bytes.pop_back();
 
+    const std::optional<std::vector<uint8_t>> decoded = cobs_decode(bytes);
+    if (!decoded.has_value()) return; // If we fail to decode, ignore the packet
+
+    // Decode the header
+    Header received_header{};
+    memcpy(&received_header, decoded->data(), sizeof(received_header));
+
+    Packet received_packet{received_header, decoded.value()};
+
+    // Ensure the packet is valid by checking the checksum
+    if (!received_packet.check_checksum()) {
+        // If the checksum is invalid, ignore the packet
+        return;
+    }
+
+
+    this->buffers[received_header.packet_id].add(received_packet);
 }
 
 std::optional<Packet> SerialHandler::pop_latest(PacketId packet_id)
--- SerialHandler.hpp
+++ SerialHandler.hpp
@@ -9,10 +9,57 @@
 #include <memory>
 #include <typeindex>
 #include <unistd.h>
+#include <libusb-1.0/libusb.h>
 
 #include "Buffer.hpp"
 #include "packet/Packet.hpp"
 
+
+/**
+ * A USB device is structured as follows:
+ * Highest level is a device, which is the actual usb, such as a mouse, headphones, etc.
+ * Each device can have multiple configurations. In our code we use libusb_get_active_config_descriptor
+ * to get the active one.
+ * A configuration contains multiple interfaces, which are used for different things. For example a
+ * webcam may have an interface for video, and another for audio.
+ * Each interface has endpoints, which is what is used to actually send and receive data. An endpoint can only
+ * send or receive data, not both. One exception is the endpoint at number 0, the control endpoint, which is required
+ * to be bidirectional.
+ * An endpoint address is 8 bits, with the MSB used as a input/output (1/0) flag. Bits 3:0 are used for the endpoint
+ * number, and the rest of the bits are reserved.
+*/
+
+// "udevadm info -a -n /dev/ttyACMx" can be used to print information about the device
+
+/** Every device under VEX has the same vendor ID */
+// This can be checked here by searching for VEX: https://the-sz.com/products/usbid/index.php
+constexpr uint16_t VEX_USB_VENDOR_ID = 0x2888;
+
+// These numbers could only change on firmware updates, but that is unlikely because it would
+// probably break other code.
+/** The communications interface. Possibly used for uploading files. Unused in our code. */
+constexpr uint8_t VEX_USB_COMMUNICATIONS_INTERFACE_NUMBER = 0x00;
+/** The input endpoint for the communications interface, with type interrupt. */
+constexpr uint8_t VEX_USB_COMMUNICATIONS_ENDPOINT_IN = 0x81;
+
+/** The interface 1 after contains the actual input and output endpoints. */
+constexpr uint8_t VEX_USB_COMMUNICATIONS_DATA_INTERFACE_NUMBER = 0x01;
+/** The input endpoint for the communications data interface, with type bulk. */
+constexpr uint8_t VEX_USB_COMMUNICATIONS_DATA_ENDPOINT_IN = 0x82;
+/** The output endpoint for the communications data interface, with type bulk. */
+constexpr uint8_t VEX_USB_COMMUNICATIONS_DATA_ENDPOINT_OUT = 0x03;
+
+/** The user interface. Used to send and receive data to the PI. */
+constexpr uint8_t VEX_USB_USER_INTERFACE_NUMBER = 0x02;
+/** The input endpoint for the user interface, with type interrupt. */
+constexpr uint8_t VEX_USB_USER_ENDPOINT_IN = 0x84;
+
+/** The interface 1 after contains the actual input and output endpoints. */
+constexpr uint8_t VEX_USB_USER_DATA_INTERFACE_NUMBER = 0x03;
+/** The input endpoint for the user data interface, with type bulk. */
+constexpr uint8_t VEX_USB_USER_DATA_ENDPOINT_IN = 0x85;
+/** The output endpoint for the user data interface, with type bulk. */
+constexpr uint8_t VEX_USB_USER_DATA_ENDPOINT_OUT = 0x06;
 
 /** An enumeration for the type of device this SerialHandler is running on. */
 enum class DeviceType {
@@ -53,14 +100,15 @@ public:
         memcpy(data_to_send.data() + sizeof(packet.header), packet.data.data(), packet.data.size());
 
         // Frame the packet using COBS
-        const std::optional<std::vector<uint8_t>> encoded = cobs_encode(data_to_send);
+        std::optional<std::vector<uint8_t>> encoded = cobs_encode(data_to_send);
         if (!encoded.has_value()) return;
 
         // Write the data to the serial connection
+        // TODO: Both of these functions have a possibility to not fully send all of the data, we would need to resend the part not sent
         if (this->device_type == DeviceType::BRAIN) {
             write(STDOUT_FILENO, encoded->data(), encoded->size());
         } else if (this->device_type == DeviceType::PI) {
-            write(this->fd, encoded->data(), encoded->size());
+            libusb_bulk_transfer(this->device_handle, VEX_USB_USER_DATA_ENDPOINT_OUT, encoded->data(), static_cast<int>(encoded->size()), nullptr, 0);
         }
     }
 
@@ -79,6 +127,6 @@ private:
     /** The type of device this SerialHandler is running on. */
     DeviceType device_type;
 
-    /** The file descriptor for the serial connection. */
-    int fd = -1;
+    /** A libusb device handle. */
+    libusb_device_handle* device_handle;
 };
