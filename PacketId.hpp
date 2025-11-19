02f12bd9339cadaf22124595d9b4870fb97ee2f0
miles <mtdaly@ncsu.edu>
2025-11-18 20:23:05 -0500

    Added IntializeOptical and InitializeOpticalComplete packets

--- PacketId.hpp
+++ PacketId.hpp
@@ -12,5 +12,12 @@ enum class PacketId : uint8_t {
     ENCODER,
 
     /** Struct to test sending packets > 512 bytes */
-    LARGE
+    LARGE,
+
+    /** A packet sent from the brain to the pi to set up the optical sensor. This is an all-in-one packet that will
+     * zero the readings, and run the calibration. */
+    INITIALIZE_OPTICAL,
+
+    /** A packet sent from the pi to the brain to signal that initialization and calibration of the optical sensor has been finished. */
+    INITIALIZE_OPTICAL_COMPLETE
 };
---
+++ packet/types/InitializeOptical.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+struct InitializeOptical
+{
+    int x;
+};
